b0569ee3795e2e98841508d8f8f75de7ce3ba351
Analice Moschen <analice.moschen16@gmail.com>
2023-06-27 11:27:17 -0300

    Alteraçoes - Enviando 2706

--- app/src/main/java/br/edu/utfpr/hotelpet/CadTutor.kt
+++ app/src/main/java/br/edu/utfpr/hotelpet/CadTutor.kt
@@ -1,7 +1,30 @@
 package br.edu.utfpr.hotelpet
 
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import br.edu.utfpr.hotelpet.databinding.ActivityCadtutorBinding
 
-class CadTutor : AppCompatActivity(R.layout.activity_cadtutor) {
+class CadTutor : AppCompatActivity() {
 
+    private val binding by lazy{
+        ActivityCadtutorBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(binding.root)
+        configCadCoTutor()
+        Log.i("CadTutor","OnCreate: Estado criado")
+    }
+
+    fun configCadCoTutor(){
+        val botaoCadCoTutor = binding.btRegistrarCoTutor
+        botaoCadCoTutor.setOnClickListener {
+            val intent = Intent(this, CadCoTutor::class.java)
+            startActivity(intent)
+        }
+    }
 }
--- app/src/main/java/br/edu/utfpr/hotelpet/FichaComportamentoPet.kt
+++ app/src/main/java/br/edu/utfpr/hotelpet/FichaComportamentoPet.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class FichaComportamentoPet : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fichacomportamentopet)
